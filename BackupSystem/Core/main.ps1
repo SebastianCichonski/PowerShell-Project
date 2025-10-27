2cb1d6b05b83f9eb49635b1b960e5fedba60f580
Sebastian Cichonski <sebqu@outlook.com>
2025-10-27 22:08:06 +0100

    zmiany w main i initialize...

--- BackupSystem/Core/main.ps1
+++ BackupSystem/Core/main.ps1
@@ -3,23 +3,26 @@ Import-module ".\Deduplication\Deduplication.psm1"
 Import-module ".\Backup\Backup.psm1"
 
 # initializacja środowiska backupu
-Initialize-BackupEnvironment 
+Initialize-BackupEnvironment
 
 # załadowanie ustawień backupu
 $backupSettings = Load-BackupSettings -settingsFilePath ".\..\Config\settings.json"
 
 # pobranie listy plików źródłowych do backupu
-$sourceFiles = Get-SourceFiles -sourceFolders $backupSettings
+$sourceFiles = Get-SourceFiles -settingsFile $backupSettings
+
+foreach ($sourceFolder in $backupSettings.SourceFolders) {
+    
+    $backupRootPath = $backupSettings.BackupRoot
+    $sourceFolder = Split-Path -path $sourceFolder.path -Leaf
+    $dedupIndexPath = Join-Path -Path $backupRootPath -ChildPath "$sourceFolder\Metadata\dedup_index.json"
 
 # załadowanie indeksu deduplikacji
-$dedupIndex = Load-DedupIndex -dedupIndexPath $backupSettings.DedupIndexPath
-
+$dedupIndex = Load-DedupIndex -dedupIndexPath $dedupIndexPath
 
 # przetworzenie każdego pliku źródłowego ($dedupIndex jest przekazywany przez referencję)
 Process-File -sourceFiles $sourceFiles -dedupIndex ([ref]$dedupIndex) -backupRoot $backupSettings.BackupRoot
 
 # zapisanie zaktualizowanego indeksu deduplikacji
 Save-DedupIndex -dedupIndex $dedupIndex -dedupIndexPath $backupSettings.DedupIndexPath
-
-    
-
+}
